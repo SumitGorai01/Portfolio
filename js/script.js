c1357e7801b246b6dea044722217bd23b69353d7
Sumit <goraisumit01@gmail.com>
2024-04-01 15:33:15 +0530

    update contact us form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-var typed = new Typed(".text",{
-    strings :["Java Developer","Software Engineer","Web Developer"],
-    typSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+var typed = new Typed(".text", {
+    strings: ["Java Developer", "Software Engineer", "Web Developer"],
+    typSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
+
+
+function collapseNavbar() {
+    var checkBox = document.getElementById("nav-check");
+    checkBox.checked = false;
+}
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "goraisumit01@gmail.com",
+        Password: "68F730BD7F008A0DC76C37C5F363DCDA6B71",
+        To: 'goraisumit01@gmail.com',
+        From:'goraisumit01@gmail.com',
+        Subject: "From Protfolio ",
+        Body: "Name : "+document.getElementById("name").value
+                    +"<br> Email : " +document.getElementById("email").value
+                    +"<br> Message : " +document.getElementById("message").value
+                    
+    }).then(
+        message => alert("Message Sent Successfully")
+    );
+}
